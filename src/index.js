396afedc15f39add6f4cefee6ff516d530590478
Arnold Paye <arnoldpayet@gmail.com>
2022-05-17 08:49:29 -0400

    fetch component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import { useFetch } from "./useFetch";
+
+function App({ login }) {
+  const { loading, data, error } = useFetch(
+    `https://api.github.com/users/${login}`
+  );
+  if (loading) return <h1>loading...</h1>;
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  return (
+    <div>
+      <img src={data.avatar_url} alt={data.login} />
+      <div>
+        <h1>{data.login}</h1>
+        {data.name && <p>{data.name}</p>}
+        {data.location && <p>{data.location}</p>}
+      </div>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <TreesContext.Provider value={{ trees }}>
-    <App />
-  </TreesContext.Provider>
-);
+root.render(<App login="arnoldpaye" />);
--- src/useFetch.js
+++ src/useFetch.js
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 
 export function useFetch(uri) {
   const [data, setData] = useState();
-  const [loading, setLoading] = useState();
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState();
 
   useEffect(() => {
